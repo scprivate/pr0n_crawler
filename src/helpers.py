6b7bc4ef27173e6a83f2e8800cbbf7fba29d5adb
Hugo Alliaume <kocal@live.fr>
2017-12-18 22:30:37 +0100

    Prepare JavaScript version of pr0n_crawler

--- src/helpers.py
+++
@@ -1,8 +0,0 @@
-def hms_to_s(hms: str) -> int:
-    """
-    Convert hh:mm:ss to sum of seconds
-    :param hms:
-    :return:
-    """
-    l = list(map(int, hms.split(':')))
-    return sum(n * sec for n, sec in zip(l[::-1], (1, 60, 3600)))
