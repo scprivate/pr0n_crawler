4bc6be9082431518860892c0e05a4f4d2dfdf3c2
Hugo Alliaume <kocal@live.fr>
2017-12-26 21:01:38 +0100

    Remove config.ts

--- config.ts
+++
@@ -1,9 +0,0 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
-export const jwtToken = isProduction
-  ? 'aaaa'
-  : 'qsdqsd';
-
-export const graphqlEndpoint = isProduction
-  ? 'ddds'
-  : 'https://example.com/graphql/';
