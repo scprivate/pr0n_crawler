46fb747a6e843e15dc058724f3cd30c506e21b32
Hugo Alliaume <kocal@live.fr>
2018-01-21 08:39:33 +0100

    replace browser's URL by NodeJS url module

--- src/sites/youjizz.ts
+++ src/sites/youjizz.ts
@@ -1,5 +1,6 @@
 import * as sec from 'sec';
 import { Site } from '../Site';
+import { resolve as resolveUrl } from 'url';
 
 class YouJizzSite extends Site {
   public getUrl(): string {
@@ -21,16 +22,17 @@ class YouJizzSite extends Site {
   public getFields(): ISiteFields {
     return {
       previousPage: {
-        selector: '(//*[contains(@class, "pagination")]/li[contains(@class, "active")]/preceding-sibling::li/a/@href)[last()]',
-        normalizer: (previousPage: string) => String(new URL(previousPage, this.getUrl())),
+        selector:
+          '(//*[contains(@class, "pagination")]/li[contains(@class, "active")]/preceding-sibling::li/a/@href)[last()]',
+        normalizer: (previousPage: string) => String(resolveUrl(this.getUrl(), previousPage)),
       },
       videosUrl: {
         selector: '//*[contains(@class, "video-title")]/a/@href',
-        normalizer: (urls: string[]) => urls.map(url => String(new URL(url, this.getUrl()))),
+        normalizer: (urls: string[]) => urls.map(url => String(resolveUrl(this.getUrl(), url))),
       },
       videosThumbnailUrl: {
         selector: '//*[contains(@class, "video-item")]/div/a/img[contains(@class, "lazy")]/@data-original',
-        normalizer: (thumbnailsUrl: string[]) => thumbnailsUrl.map(url => String(new URL(url, this.getUrl()))),
+        normalizer: (thumbnailsUrl: string[]) => thumbnailsUrl.map(url => String(resolveUrl(this.getUrl(), url))),
       },
       video: {
         title: {
